fda15164838fa04fbac089b7ccb8ec50597263ca
handwithfivefingers <handgod1995@gmail.com>
2023-03-27 18:38:48 +0700

    update husky

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,28 @@
 module.exports = {
-  stories: ["../src/**/*.mdx", "../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', "@storybook/addon-mdx-gfm"],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {}
-  },
-  features: {
-    storyStoreV7: true
-  },
-  async viteFinal(config) {
-    return {
-      ...config,
-      define: {
-        ...config.define,
-        global: 'window'
-      }
-    };
-  },
-  docs: {
-    autodocs: true
-  }
-};
+    stories: ['../src/**/*.mdx', '../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-mdx-gfm',
+    ],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
+    },
+    features: {
+        storyStoreV7: true,
+    },
+    async viteFinal(config) {
+        return {
+            ...config,
+            define: {
+                ...config.define,
+                global: 'window',
+            },
+        }
+    },
+    docs: {
+        autodocs: false,
+    },
+}
--- src/components/Form/Select/Select.stories.tsx
+++ src/components/Form/Select/Select.stories.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import { Meta, StoryFn } from '@storybook/react';
-import Select from './Select';
+import React from 'react'
+import { Meta, StoryFn } from '@storybook/react'
+import Select from './Select'
 
 export default {
-	title: 'Form/Select',
-	component: Select,
-} as Meta<typeof Select>;
+    title: 'Form/Select',
+    component: Select,
+} as Meta<typeof Select>
 
-const Template: StoryFn<typeof Select> = (args) => {
-	return <Select {...args} />;
-};
+const Template: StoryFn<typeof Select> = args => {
+    return <Select {...args} />
+}
 
-export const Default = Template.bind({});
-export const NoProps = Template.bind({});
+export const Default = Template.bind({})
+export const Empty = Template.bind({})
 
 Default.args = {
-	label: 'Label',
-	name: ['user', 'name'],
-	onChange: (value) => console.log('value', value),
-	options: [
-		{
-			label: 'Options 1',
-			value: ['option', '1'],
-		},
-		{
-			label: 'Options 2',
-			value: ['option', '2'],
-		},
-		{
-			label: 'Options 3',
-			value: ['option', '3'],
-		},
-		{
-			label: 'Options 4',
-			value: ['option', '4'],
-		},
-	],
-};
+    label: 'Label',
+    name: ['user', 'name'],
+    onChange: value => console.log('value', value),
+    options: [
+        {
+            label: 'Options 1',
+            value: ['option', '1'],
+        },
+        {
+            label: 'Options 2',
+            value: ['option', '2'],
+        },
+        {
+            label: 'Options 3',
+            value: ['option', '3'],
+        },
+        {
+            label: 'Options 4',
+            value: ['option', '4'],
+        },
+    ],
+}
 
-NoProps.args = {
-	onChange: (value) => console.log('noProps', value),
-};
+Empty.args = {
+    onChange: value => console.log('noProps', value),
+}
